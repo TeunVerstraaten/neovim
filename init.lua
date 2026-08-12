76b7f54c79fc8039447ea6fc5963ffdf2363d986
tverstraaten <twverstraaten@gmail.com>
2026-08-12 13:17:16 +0200

    Change find word key bind

--- init.lua
+++ init.lua
@@ -376,7 +376,7 @@ local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find keymaps" })
-vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Find word" })
+vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Find word" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help" })
 vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Find recent files" })
